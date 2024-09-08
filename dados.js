137a0d63d1d2771cdc5c0d191aef6184a17bc171
AndreFerrazzzz <andreflasse76@gmail.com>
2024-09-08 15:45:58 -0300

    Terceiro Commit, iniciando aula 04

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [  //Array de objetos
+    {
+      titulo: "Rayssa Leal",
+      descricao: 'é uma ginasta brasileira que conquistou o coração dos brasileiros com sua determinação, talento e alegria. Com uma carreira marcada por superações e conquistas históricas, ela se tornou um dos maiores nomes do esporte brasileiro.',
+      link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal'
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: 'é uma judoca brasileira conhecida por sua técnica impecável e espírito de luta. Conquistou diversas medalhas em competições nacionais e internacionais, sendo um grande exemplo para a nova geração de judocas.',
+      link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza' 
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: 'é uma ginasta brasileira que conquistou a medalha de prata no individual geral nos Jogos Olímpicos de Tóquio 2020. Sua história de superação e talento a tornou um ícone do esporte brasileiro.',
+      link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade'
+    }
+];
+

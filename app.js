fdb92ac6c3c61e4bb0019a18dbaa937d83bf6bd5
AndreFerrazzzz <andreflasse76@gmail.com>
2024-09-08 19:38:55 -0300

    Projeto Finalizado

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ function pesquisar() {
 
     let campoPesquisa = document.querySelector('#campo-pesquisa').value
 
+    if (campoPesquisa == '') {
+        section.innerHTML = 'Nada foi encontrado'
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
     // Itera sobre cada dado no array de dados
     for (let dado of dados) {
-        
-        // Condção para pesquisar pelo atleta específico
-        if (dado.titulo.includes(campoPesquisa)) {
+            titulo = dado.titulo.toLocaleLowerCase()
+            descricao = dado.descricao.toLocaleLowerCase()
+            tags = dado.tags.toLocaleLowerCase()
+
+        // Condção para pesquisar pelo atleta específico pelo nome descrção ou palavra-chave
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
             // Cria um novo elemento
             // Constrói o HTML de cada resultado, utilizando template literals (``) para facilitar a interpolação
             resultados += `
@@ -24,7 +37,15 @@ function pesquisar() {
       </div>`;
         }
     }
- // Atribui o HTML construído à seção de resultados, substituindo o conteúdo anterior
+
+    //Se o resultado não tiver nada/ não existir  
+    if (!resultados){
+        section.innerHTML = '<p>Nada foi encontrado. Você precisa digitar um nome de atleta ou esporte</p>'
+        return
+    }
+
+
+    // Atribui os resultados gerados na seção HTML
     section.innerHTML = resultados;
 }
 
--- dados.js
+++ dados.js
@@ -2,17 +2,20 @@ let dados = [  //Array de objetos
     {
       titulo: "Rayssa Leal",
       descricao: 'é uma ginasta brasileira que conquistou o coração dos brasileiros com sua determinação, talento e alegria. Com uma carreira marcada por superações e conquistas históricas, ela se tornou um dos maiores nomes do esporte brasileiro.',
-      link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal'
+      link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+      tags: "skate fada sk8"
     },
     {
       titulo: "Beatriz Souza",
       descricao: 'é uma judoca brasileira conhecida por sua técnica impecável e espírito de luta. Conquistou diversas medalhas em competições nacionais e internacionais, sendo um grande exemplo para a nova geração de judocas.',
-      link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza' 
+      link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza' ,
+      tags: "judo judô judoka"
     },
     {
       titulo: "Rebeca Andrade",
       descricao: 'é uma ginasta brasileira que conquistou a medalha de prata no individual geral nos Jogos Olímpicos de Tóquio 2020. Sua história de superação e talento a tornou um ícone do esporte brasileiro.',
-      link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade'
-    }
+      link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade',
+      tags: "ginastica ouro ginasta"
+    },
 ];
 

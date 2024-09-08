cedff63f85b8114fde0e7594ef6175953bf11b01
AndreFerrazzzz <andreflasse76@gmail.com>
2024-09-08 17:03:07 -0300

    Quarto commit, finalizando aula 04

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log(dados)
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.querySelector('#resultados-pesquisa');
+  
+    // Inicializa uma string vazia para armazenar os resultados 
+    let resultados = '';
+  
+    // Itera sobre cada dado no array de dados
+    for (let dado of dados) {
+      // Constrói o HTML de cada resultado, utilizando template literals (``) para facilitar a interpolação
+      resultados += `
+        <div class="item-resultado"> 
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta"> ${dado.descricao}</p>
+          <a href=${dado.link} target="_blank">Saiba mais!</a>
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML construído à seção de resultados, substituindo o conteúdo anterior
+    section.innerHTML = resultados;
+  }
